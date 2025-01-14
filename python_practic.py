33e797ed19e497656a38ce8ecf2842f260c5ba9b
the-coderRavi <ravigoku7061@gmail.com>
2025-01-14 22:27:33 +0530

    day one practic after sleep

--- python_practic.py
+++ python_practic.py
@@ -1,3 +1,55 @@
 print(" hello my love ")
 
-print("hye i am ravi jaiswal and \ni was learning about "python programming)
+print("hye i want to know about \"python language\"  and this project is my project")
+
+print("hello my life", "i want to know about \"python language\"\nand this project is my project", sep="~")
+print( 6 , 7 , 8 , 9 ,  sep="~" ,  end="56789")
+
+a=1
+b=6
+c=7
+d=8
+print("a+b+c+")
+print("this type of a is " , type(a))
+print("this type of b is " , type(b))
+print("this type of c is " , type(c))
+print("this type of d is " , type(d))
+
+print(a+b+c+d)
+
+print(15+6)
+print(19*6)
+print(28-20)
+print(30/6)
+print(30//6)
+print(30%6)
+print(66**7)
+
+# hye at the time i was write a coad for calculator function
+
+print("calulator")
+
+while True:
+    choice = input("Enter your choice: ")
+    if choice == '1':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        print("Addition result:", num1 + num2)
+    elif choice == '2':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        print("Subtraction result:", num1 - num2)
+    elif choice == '3':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        print("Multiplication result:", num1 * num2)
+    elif choice == '4':
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        print("Division result:", num1 / num2)
+    elif choice == '5':
+        print("Exiting the program.")
+        break
+    else:
+         print("invalid choice , pease slect a  valid option.")
+
